9c041b7dfbfb2c87b505bff38975dd77c7b620fd
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-10-12 16:56:33 +0530

    Refactor database configuration file

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
+
+
+  // these are some more changes to test the system
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
